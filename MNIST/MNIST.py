ee8f073920f30998aa4b65d33e61b778c13a7d1f
Lucas <lucascorreadeassis@gmail.com>
2021-01-19 12:03:09 -0300

    comments fix

--- MNIST/MNIST.py
+++ MNIST/MNIST.py
@@ -6,7 +6,7 @@ import torch
 from torchvision import datasets, transforms
 
 class MNIST:
-    '''MNIST possui 10 classes, os digitos entre 0 e 9!'''
+    '''MNIST have 10 classes (digits from 0 to 9)'''
     def __init__(self):
         self.mnistDataset = datasets.MNIST('./dataset',
                                train = False,
@@ -15,15 +15,15 @@ class MNIST:
 
     def printDatasetInfo(self):
         '''Print data and target types, data size and target value'''
-        dado, rotulo = self.mnistDataset[0]
-        print(type(dado), type(rotulo))
-        print(dado.size(), rotulo)
+        data, target = self.mnistDataset[0]
+        print(type(data), type(target))
+        print(data.size(), target)
 
     def plotDatasetSamples(self):
         '''Plot 10 first samples from the dataset'''
         fig, axs = plt.subplots(1, 10, figsize=(15,4))
         for i in range(10):
-            dado, rotulo = self.mnistDataset[i]
-            axs[i].imshow(dado[0], cmap='gray')
-            axs[i].set_title(str(rotulo))
+            data, target = self.mnistDataset[i]
+            axs[i].imshow(data[0], cmap='gray')
+            axs[i].set_title(str(target))
         plt.show()
